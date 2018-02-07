03dbc5c35527a1c9347be233c0face02285fc60e
closedsum <closedsum@gmail.com>
2018-02-07 15:47:01 -0800

    Adding Test, Set, Clear BitMask Macro
    Adding Test, Set, Clear BitMask Macro

--- CsCore/Types/CsTypes_Macro.h
+++ CsCore/Types/CsTypes_Macro.h
@@ -9,3 +9,7 @@
 #define CS_TEST_BLUEPRINT_BITFLAG(Bitmask, Bit) (((Bitmask) & (1 << static_cast<uint32>(Bit))) > 0)
 #define CS_SET_BLUEPRINT_BITFLAG(Bitmask, Bit) (Bitmask |= 1 << static_cast<uint32>(Bit))
 #define CS_CLEAR_BLUEPRINT_BITFLAG(Bitmask, Bit) (Bitmask &= ~(1 << static_cast<uint32>(Bit)))
+
+#define CS_TEST_BITFLAG(Bitmask, Bit) (Bitmask & Bit) == Bit
+#define CS_SET_BITFLAG(Bitmask, Bit) Bitmask |= Bit
+#define CS_CLEAR_BITFLAG(Bitmask, Bit) Bitmask &= ~Bit

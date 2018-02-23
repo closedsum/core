6ca0ae062e694d49b9d2a7e3655743b6117e447f
closedsum <closedsum@gmail.com>
2018-02-23 00:45:10 -0800

    Stubbing in Data for Character

---
+++ CsCore/Data/CsData_Character.cpp
@@ -0,0 +1,8 @@
+// Copyright 2017-2018 Closed Sum Games, LLC. All Rights Reserved.
+#include "Data/CsData_Character.h"
+#include "CsCore.h"
+
+ACsData_Character::ACsData_Character(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}

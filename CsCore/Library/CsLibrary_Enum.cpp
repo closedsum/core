0c31194f9613eb6d84728883c6b6f65ac7a855ad
closedsum <closedsum@gmail.com>
2018-05-29 19:47:15 -0700

    MBO: Adding Blueprint Library for EnumStructs

---
+++ CsCore/Library/CsLibrary_Enum.cpp
@@ -0,0 +1,8 @@
+// Copyright 2017-2018 Closed Sum Games, LLC. All Rights Reserved.
+#include "Library/CsLibrary_Enum.h"
+#include "CsCore.h"
+
+UCsLibrary_Enum::UCsLibrary_Enum(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
---
+++ CsCore/Library/CsLibrary_Enum.h
@@ -0,0 +1,10 @@
+// Copyright 2017-2018 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CsLibrary_Enum.generated.h"
+
+UCLASS()
+class CSCORE_API UCsLibrary_Enum : public UBlueprintFunctionLibrary
+{
+	GENERATED_UCLASS_BODY()
+};

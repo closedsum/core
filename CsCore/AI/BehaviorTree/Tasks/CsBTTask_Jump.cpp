a2887e2e4c1a706f6cc8392d1eadd8ee9e9977c7
closedsum <closedsum@gmail.com>
2018-08-24 14:20:30 -0700

    MBO: AI Jumping

--- CsCore/AI/BehaviorTree/Tasks/CsBTTask_Jump.cpp
+++ CsCore/AI/BehaviorTree/Tasks/CsBTTask_Jump.cpp
@@ -19,6 +19,11 @@ UCsBTTask_Jump::UCsBTTask_Jump(const FObjectInitializer& ObjectInitializer)
 {
 	NodeName = TEXT("Jump");
 	bNotifyTick = true;
+
+	// accept only actors, vectors, and rotators
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, BlackboardKey), AActor::StaticClass());
+	BlackboardKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, BlackboardKey));
+	BlackboardKey.AddRotatorFilter(this, GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, BlackboardKey));
 }
 
 EBTNodeResult::Type UCsBTTask_Jump::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
@@ -93,3 +98,87 @@ void UCsBTTask_Jump::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemo
 		}
 	}
 }
+
+EBTNodeResult::Type UCsBTTask_Jump::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (AAIController* AIController = OwnerComp.GetAIOwner())
+	{
+		if (ACsAIPawn* Pawn = Cast<ACsAIPawn>(AIController->GetPawn()))
+		{
+#if !UE_BUILD_SHIPPING
+			if (CsCVarLogAIBTTasks->GetInt() == CS_CVAR_SHOW_LOG)
+			{
+				UBehaviorTree* BTree = OwnerComp.GetCurrentTree();
+
+				UE_LOG(LogCs, Warning, TEXT("UCsBTTask_Jump::AbortTask (%s.%s): Aborted."), *(Pawn->GetName()), *(BTree->GetName()));
+			}
+#endif // #if !UE_BUILD_SHIPPING
+		}
+	}
+	return EBTNodeResult::Aborted;
+}
+
+#if WITH_EDITOR
+
+void UCsBTTask_Jump::PostEditChangeProperty(struct FPropertyChangedEvent& e)
+{
+	FName PropertyName = (e.Property != NULL) ? e.Property->GetFName() : NAME_None;
+	/*
+	// Type
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, Type))
+	{
+		SetTimeByType();
+
+		Super::PostEditChangeProperty(e);
+		return;
+	}
+	// bTime
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, Type))
+	{
+		if (bTime)
+		{
+			Type = ECsBTTask_LookAtType::Timed;
+
+			if (Time <= 0.0f)
+				Time = 1.0f;
+		}
+		else
+		{
+			if (Type == ECsBTTask_LookAtType::Timed)
+				Type = ECsBTTask_LookAtType::UntilAligned;
+
+			if (Time > 0.0f)
+				Time = 0.0f;
+		}
+
+		Super::PostEditChangeProperty(e);
+		return;
+	}
+	// Time
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(UCsBTTask_Jump, Time))
+	{
+		if (Time < 0.0f)
+		{
+			bTime = false;
+			Type = ECsBTTask_LookAtType::Forever;
+		}
+		else
+			if (Time > 0.0f)
+			{
+				bTime = true;
+				Type = ECsBTTask_LookAtType::Timed;
+			}
+			else
+			{
+				bTime = false;
+				Type = ECsBTTask_LookAtType::UntilAligned;
+			}
+
+		Super::PostEditChangeProperty(e);
+		return;
+	}
+	*/
+	Super::PostEditChangeProperty(e);
+}
+
+#endif // #if WITH_EDITOR
--- CsCore/AI/BehaviorTree/Tasks/CsBTTask_Jump.h
+++ CsCore/AI/BehaviorTree/Tasks/CsBTTask_Jump.h
@@ -1,21 +1,40 @@
 // Copyright 2017-2018 Closed Sum Games, LLC. All Rights Reserved.
 #pragma once
 
-#include "Runtime/AIModule/Classes/BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "CsBTTask_Jump.generated.h"
 
 UCLASS(config = Game)
-class CSCORE_API UCsBTTask_Jump : public UBTTaskNode
+class CSCORE_API UCsBTTask_Jump : public UBTTask_BlackboardBase
 {
 	GENERATED_UCLASS_BODY()
 
 protected:
 
-	//UPROPERTY(EditAnywhere, Category = Node)
-	//bool bForever;
+	UPROPERTY(EditAnywhere, Category = Node)
+	bool bTime;
+	UPROPERTY(EditAnywhere, Category = Node)
+	float Time;
+
+	UPROPERTY(EditAnywhere, Category = Node)
+	bool bHeight;
+	UPROPERTY(EditAnywhere, Category = Node)
+	float Height;
+
+	UPROPERTY(EditAnywhere, Category = Node)
+	bool bSpeedAsPercent;
+	UPROPERTY(EditAnywhere, Category = Node)
+	float SpeedAsPercent;
 
 public:
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+#if WITH_EDITOR
+
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& e) override;
+
+#endif // #if WITH_EDITOR
 };

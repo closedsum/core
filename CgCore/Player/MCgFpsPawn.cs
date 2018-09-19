39f2a5d35f4bc3e9ee02e88e07989f9f4391e931
closedsum <closedsum@gmail.com>
2018-09-19 16:40:00 -0700

    Unity: Updating FpsPawn

--- CgCore/Player/MCgFpsPawn.cs
+++ CgCore/Player/MCgFpsPawn.cs
@@ -1,16 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace CgCore
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class MCgFpsPawn : MonoBehaviour {
+    public class MCgFpsPawn : MCgPlayerPawn
+    {
+        #region "Data Members"
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public Transform Eye;
+
+        public SkinnedMeshRenderer Mesh1P;
+
+            #region "Camera"
+
+        public float EyeHeight;
+
+        #endregion // Camera
+
+        #endregion // Data Members
+
+        #region "Camera"
+
+        public virtual Vector3 GetPawnViewLocation()
+        {
+            return Vector3.zero;
+        }
+
+        public virtual Vector3 GetFeetLocation()
+        {
+            return Vector3.zero;
+        }
+
+        #endregion // Camera
+
+        #region "Mesh"
+
+        public virtual SkinnedMeshRenderer GetCurrentMesh()
+        {
+            return null;
+        }
+
+        #endregion // Mesh
+
+        #region "Weapons"
+
+        public virtual void ApplyData_Weapon()
+        {
+
+        }
+
+        #endregion // Weapons
+    }
 }

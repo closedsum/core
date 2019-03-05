92486a0d2cf02cac8920f2e291eb28df235243d9
closedsum <closedsum@gmail.com>
2019-03-04 18:53:01 -0800

    Stubbing in EnumEditor Util

---
+++ CsEditor/CsEnumEditorUtils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class UUserDefinedEnum;
+
+class CSEDITOR_API FCsEnumEditorUtils
+{
+public:
+
+	static void SyncInputAction();
+	static void UpdateNames(const FName& UserDefinedEnumObjectPath, const TArray<FName>& Names, const TMap<FName, FText>& DisplayNameMap);
+	static void PrepareForChange(UUserDefinedEnum* Enum);
+	static void BroadcastChanges(const UUserDefinedEnum* Enum, const TArray<TPair<FName, int64>>& OldNames, bool bResolveData = true);
+
+	/** copy full enumerators names from given enum to OutEnumNames, the last '_MAX' enumerator is skipped */
+	static void CopyEnumeratorsWithoutMax(const UEnum* Enum, TArray<TPair<FName, int64>>& OutEnumNames);
+
+	static FName GetNameFromNamespacedName(const FName& Name);
+};

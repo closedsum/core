bb7081ab773d6b89446bd98670c71a2b80929a97
beditheory <beditheory@gmail.com>
2019-12-18 23:45:27 -0800

    WIP Manager TArray

--- CsPlugin/Source/CsCore/Managers/TArray/CsManager_TArray.cpp
+++ CsPlugin/Source/CsCore/Managers/TArray/CsManager_TArray.cpp
@@ -3,49 +3,54 @@
 #include "CsCore.h"
 #include "CsCVars.h"
 
+// Enums
+#pragma region
+
+	// ManagerTArraySize
+#pragma region
+
+namespace NCsManagerTArraySize
+{
+	namespace Ref
+	{
+		CSCORE_API CS_ADD_TO_ENUM_MAP(EMCsManagerTArraySize, Small);
+		CSCORE_API CS_ADD_TO_ENUM_MAP(EMCsManagerTArraySize, Medium);
+		CSCORE_API CS_ADD_TO_ENUM_MAP(EMCsManagerTArraySize, Large);
+		CSCORE_API CS_ADD_TO_ENUM_MAP_CUSTOM(EMCsManagerTArraySize, VeryLarge, "Very Large");
+		CSCORE_API CS_ADD_TO_ENUM_MAP_CUSTOM(EMCsManagerTArraySize, ECsManagerTArraySize_MAX, "MAX");
+	}
+}
+
+#pragma endregion ManagerTArraySize
+
+#pragma endregion Enums
+
 FCsManager_TArray::FCsManager_TArray()
 {
 }
 
 FCsManager_TArray::~FCsManager_TArray()
 {
-	Manager_TArray_bool.Reserve((uint8)ECsManagerTArraySize::ECsManagerTArraySize_MAX);
-	Manager_TArray_bool[(uint8)ECsManagerTArraySize::S_8] = &Manager_TArray_bool_8;
+	Sizes.Reserve((uint8)ECsManagerTArraySize::ECsManagerTArraySize_MAX);
+	Sizes.Add(8);
+	Sizes.Add(64);
+	Sizes.Add(128);
+	Sizes.Add(512);
+
+	//std::is
 }
 
 TArray<bool>& FCsManager_TArray::AllocateRef_TArray_bool(const ECsManagerTArraySize& Size)
 {
-	int32 Max = 8;
-	TArray<bool>* Ptr = nullptr;
-
-	// 8
-	if (Size == ECsManagerTArraySize::S_8)
-	{
-		Max = 8;
-		Ptr	= Manager_TArray_bool_8.AllocateResource();
-	}
-
-	checkf(Ptr->Max() == Max, TEXT(""));
-
-	Ptr->Reset(Ptr->Max());
-
-	return *Ptr;
+	return Pool_bool.AllocateRef_TArray(Size);
 }
 
 TArray<bool>& FCsManager_TArray::ForceAllocateRef_TArray_bool(const ECsManagerTArraySize& Size)
 {
-	// 8
-	if (Size == ECsManagerTArraySize::S_8)
-	{
-		if (Manager_TArray_bool_8.IsExhausted())
-			Manager_TArray_bool_8.DellocateHead();
-	}
-	return AllocateRef_TArray_bool(Size);
+	return Pool_bool.ForceAllocateRef_TArray(Size);
 }
 
 void FCsManager_TArray::DeallocateAll_TArray_bool(const ECsManagerTArraySize& Size)
 {
-	// 8
-	if (Size == ECsManagerTArraySize::S_8)
-		Manager_TArray_bool_8.DeallocateAll();
+	Pool_bool.DeallocateAll_TArray(Size);
 }
--- CsPlugin/Source/CsCore/Managers/TArray/CsManager_TArray.h
+++ CsPlugin/Source/CsCore/Managers/TArray/CsManager_TArray.h
@@ -2,53 +2,147 @@
 #pragma once
 
 #include "Managers/MemoryResource/CsManager_MemoryResource_Fixed.h"
+#include "Types/CsTypes_Primitive.h"
+#include <typeinfo>
+
+// Enums
+#pragma region
+
+	// ManagerTArraySize
+#pragma region
 
 enum class ECsManagerTArraySize
 {
-	S_8,
-	S_64,
-	S_128,
-	S_512,
-	ECsManagerTArraySize_MAX
+	Small,
+	Medium,
+	Large,
+	VeryLarge,
+	ECsManagerTArraySize_MAX,
 };
+
+struct CSCORE_API EMCsManagerTArraySize final : public TCsEnumMap<ECsManagerTArraySize>
+{
+	CS_ENUM_MAP_BODY_WITH_EXPLICIT_MAX(EMCsManagerTArraySize, ECsManagerTArraySize)
+};
+
+namespace NCsManagerTArraySize
+{
+	typedef ECsManagerTArraySize Type;
+
+	namespace Ref
+	{
+		extern CSCORE_API const Type Small;
+		extern CSCORE_API const Type Medium;
+		extern CSCORE_API const Type Large;
+		extern CSCORE_API const Type VeryLarge;
+		extern CSCORE_API const Type ECsManagerTArraySize_MAX;
+	}
+}
+
+#pragma endregion ManagerTArraySize
+
+#pragma endregion Enums
 
 #define CS_MANAGER_TARRAY_POOL_SIZE 4
 
 // Structs
 #pragma region
 
-#define CS_DECLARE_MANAGER_TARRAY(ValueType, Size, PoolSize) \
-	struct FCsMemoryResource_TArray_##ValueType##_##Size : public TCsMemoryResource<TArray<ValueType>> \
+#define CS_DECLARE_MANAGER_TARRAY_TYPE(ValueType, PoolSize) \
+	struct FCsMemoryResource_TArray_##ValueType : public TCsMemoryResource<TArray<ValueType>> \
 	{ \
 	}; \
 	\
-	struct FCsManager_TArray_##ValueType##_##Size : public TCsManager_MemoryResource_Fixed<TArray<bool>, FCsMemoryResource_TArray_##ValueType##_##Size, PoolSize> \
+	struct FCsManager_TArray_##ValueType : public TCsManager_MemoryResource_Fixed<TArray<bool>, FCsMemoryResource_TArray_##ValueType, PoolSize> \
 	{ \
-	private: \
-	\
-		typedef TCsManager_MemoryResource_Fixed<TArray<bool>, FCsMemoryResource_TArray_##ValueType##_##Size, PoolSize> Super; \
-	\
-	public: \
-	\
-		FCsManager_TArray_##ValueType##_##Size() : \
-			Super() \
-		{ \
-			const TArray<FCsMemoryResource_TArray_##ValueType##_##Size*>& Array = GetPool(); \
-	\
-			for (FCsMemoryResource_TArray_##ValueType##_##Size* M : Array) \
-			{ \
-				TArray<ValueType>* R = M->Get(); \
-				R->Reserve(Size); \
-			} \
-		} \
 	};
 
-// FCsManager_TArray_bool_8
-CS_DECLARE_MANAGER_TARRAY(bool, 8, CS_MANAGER_TARRAY_POOL_SIZE)
-// FCsManager_TArray_bool_64
-CS_DECLARE_MANAGER_TARRAY(bool, 64, CS_MANAGER_TARRAY_POOL_SIZE)
+// FCsManager_TArray_bool
+CS_DECLARE_MANAGER_TARRAY_TYPE(bool, CS_MANAGER_TARRAY_POOL_SIZE)
 
 #pragma endregion Structs
+
+template<typename ManagerResourceType, typename ResourceContainerType, typename ValueType>
+class TCsManager_TArray_ValueType
+{
+public:
+
+	TArray<int32> Sizes;
+
+	TCsManager_TArray_ValueType() :
+		Sizes()
+	{
+		Sizes.Reserve((uint8)ECsManagerTArraySize::ECsManagerTArraySize_MAX);
+		Sizes.Add(8);
+		Sizes.Add(64);
+		Sizes.Add(128);
+		Sizes.Add(512);
+
+		Pool.Reserve((uint8)ECsManagerTArraySize::ECsManagerTArraySize_MAX);
+
+		for (const ECsManagerTArraySize& Size : EMCsManagerTArraySize::Get())
+		{
+			ManagerResourceType* M = new ManagerResourceType();
+
+			const TArray<ResourceContainerType*>& Array = M->GetPool();
+
+			for (ResourceContainerType* C : Array)
+			{
+				TArray<ValueType>* R = C->Get();
+				R->Reserve(Sizes[(uint8)Size]);
+			}
+
+			Pool.Add(M);
+		}
+	}
+
+	~TCsManager_TArray_ValueType()
+	{
+		const int32 Count = Pool.Num();
+
+		for (int32 I = 0; I < Count; ++I)
+		{
+			ManagerResourceType* P = Pool[I];
+
+			delete P;
+
+			Pool[I] = nullptr;
+		}
+		Pool.Reset();
+	}
+
+protected:
+
+	TArray<ManagerResourceType*> Pool;
+
+public:
+
+	TArray<ValueType>& AllocateRef_TArray(const ECsManagerTArraySize& Size)
+	{
+		const uint8 Index	   = (uint8)Size;
+		const int32& Max	   = Sizes[Index];
+		TArray<ValueType>* Ptr = Pool[Index]->AllocateResource();
+
+		checkf(Ptr->Max() == Max, TEXT("TCsManager_TArray::AllocateRef_TArray: Array Max has been altered."));
+
+		Ptr->Reset(Ptr->Max());
+
+		return *Ptr;
+	}
+
+	TArray<ValueType>& ForceAllocateRef_TArray(const ECsManagerTArraySize& Size)
+	{
+		if (Pool[(uint8)Size]->IsExhausted())
+			Pool[(uint8)Size]->DellocateHead();
+
+		return AllocateRef_TArray(Size);
+	}
+
+	void DeallocateAll_TArray(const ECsManagerTArraySize& Size)
+	{
+		Pool[(uint8)Size]->DeallocateAll();
+	}
+};
 
 class CSCORE_API FCsManager_TArray
 {
@@ -58,16 +152,25 @@ public:
 
 	virtual ~FCsManager_TArray();
 
+	TArray<int32> Sizes;
+
 // bool
 #pragma region
 protected:
 
-	TArray<ICsManager_MemoryResource*> Manager_TArray_bool;
-
-	FCsManager_TArray_bool_8 Manager_TArray_bool_8;
-	FCsManager_TArray_bool_64 Manager_TArray_bool_64;
+	TCsManager_TArray_ValueType<FCsManager_TArray_bool, FCsMemoryResource_TArray_bool, bool> Pool_bool;
 
 public:
+
+	template<typename ValueType>
+	TArray<ValueType>& AllocateRef_TArray(const ECsManagerTArraySize& Size)
+	{
+		// bool
+		if (typeid(ValueType) == typeid(bool))
+		{
+			return Pool_bool.AllocateRef_TArray(Size);
+		}
+	}
 
 	TArray<bool>& AllocateRef_TArray_bool(const ECsManagerTArraySize& Size);
 	TArray<bool>& ForceAllocateRef_TArray_bool(const ECsManagerTArraySize& Size);

e9357790c9594ce6998d21f146da84b454248352
closedsum <closedsum@gmail.com>
2020-04-27 16:54:56 -0700

    Adding ScriptLibrary_Common

---
+++ CsPlugin/Source/CsCore/Library/Script/CsScriptLibrary_Common.h
@@ -0,0 +1,17 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+
+#include "UObject/Object.h"
+#include "CsScriptLibrary_Common.generated.h"
+
+class UGameInstance;
+
+UCLASS()
+class CSCORE_API UCsScriptLibrary_Common : public UObject
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	static UGameInstance* GetGameInstanceFromContext(const FString& Context, const UObject* WorldContextObject);
+};

5365d03765be72b85de57225714fef547b62b853
closedsum <closedsum@gmail.com>
2020-05-09 18:32:56 -0700

    Stubbing in interface for spawner

---
+++ CsTowerDefense/Source/CsTd/Spawner/Creep/CsTdSpawner_Creep.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Spawner/Creep/CsTdSpawner_Creep.h"
+#include "CsTd.h"
+
+UCsTdSpawner_Creep::UCsTdSpawner_Creep(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ CsTowerDefense/Source/CsTd/Spawner/Creep/CsTdSpawner_Creep.h
@@ -0,0 +1,20 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+
+#include "UObject/Interface.h"
+#include "CsTdSpawner_Creep.generated.h"
+
+UINTERFACE(BlueprintType, meta = (CannotImplementInterfaceInBlueprint))
+class CSTD_API UCsTdSpawner_Creep : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class CSTD_API ICsTdSpawner_Creep
+{
+	GENERATED_IINTERFACE_BODY()
+
+	virtual void Stub() = 0;
+
+public:
+};

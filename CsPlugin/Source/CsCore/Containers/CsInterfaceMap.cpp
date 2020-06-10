b2d2f465c5de2a1d90d120f8b8975bf91eba8709
closedsum <closedsum@gmail.com>
2020-06-10 11:11:32 -0700

    Missed in last checkin

---
+++ CsPlugin/Source/CsCore/Containers/CsInterfaceMap.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Containers/CsInterfaceMap.h"
+#include "CsCore.h"
+
+UCsGetInterfaceMap::UCsGetInterfaceMap(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}

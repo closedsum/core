f248c0a074fc4745f77ec69d3c7cbf8b67ee264f
closedsum <closedsum@gmail.com>
2020-06-17 15:26:30 -0700

    Adding StartPlay interface

---
+++ CsPlugin/Source/CsCore/Start/CsStartPlay.cpp
@@ -0,0 +1,61 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+
+#include "Start/CsStartPlay.h"
+#include "CsCore.h"
+
+UCsStartPlay::UCsStartPlay(const class FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+// FCsStartPlay
+#pragma region
+
+const FCsStartPlay FCsStartPlay::Empty;
+
+FCsStartPlay::FCsStartPlay() :
+	Super(),
+	Script_StartPlay_Impl(),
+	Script_HasStartedPlay_Impl()
+{
+}
+
+void FCsStartPlay::SetObject(UObject* InObject)
+{
+	Super::SetObject(InObject);
+
+	if (Object)
+	{
+		UClass* Class = Object->GetClass();
+
+		// ICsStartPlay
+		{
+			// Interface
+			if (ICsStartPlay* O = Cast<ICsStartPlay>(Object))
+			{
+				SetInterface(O);
+			}
+			// Script Interface
+			else
+			if (Class->ImplementsInterface(UCsStartPlay::StaticClass()))
+			{
+				SetScript();
+			}
+			else
+			{
+				checkf(false, TEXT("FCsStartPlay:SetObject: Object: %s with Class: %s does NOT implement the interface: ICsStartPlay."), *(Object->GetName()));
+			}
+		}
+	}
+}
+
+void FCsStartPlay::Reset()
+{
+	Super::Reset();
+
+	Script_StartPlay_Impl.Unbind();
+	Script_HasStartedPlay_Impl.Unbind();
+}
+
+#pragma endregion FCsStartPlay

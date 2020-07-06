fa3c4473d1732710919b23adbab9acdbdf985704
closedsum <closedsum@gmail.com>
2020-07-06 10:40:10 -0700

    Updating Scope Group for Coroutines

--- CsPlugin/Source/CsCore/Coroutine/CsCVars_Coroutine.cpp
+++ CsPlugin/Source/CsCore/Coroutine/CsCVars_Coroutine.cpp
@@ -15,6 +15,15 @@ TAutoConsoleVariable<int32> CsCVarLogCoroutineRunning(
 	ECVF_SetByConsole
 );
 
+// Scoped Timer
+
+TAutoConsoleVariable<int32> CsCVarLogCoroutinesScopedTimer(
+	TEXT("log.coroutines.scopedtimer"),
+	0,
+	TEXT("[Non-Shipping] Log Coroutines Scoped Timer"),
+	ECVF_SetByConsole
+);
+
 TAutoConsoleVariable<int32> CsCVarLogRoutineScopedTimer(
 	TEXT("log.routine.scopedtimer"),
 	0,
@@ -36,6 +45,8 @@ namespace NCsCVarLog
 {
 	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogCoroutineTransactions, "Log Coroutine Transactions");
 	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogCoroutineRunning, "Log Coroutine Running");
+	// Scoped Timer
+	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogCoroutinesScopedTimer, "Log Coroutines Scoped Timer");
 	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogRoutineScopedTimer, "Log Routine Scoped Timer");
 	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogCoroutineScopedTimer, "Log Coroutine Scoped Timer");
 
@@ -43,6 +54,8 @@ namespace NCsCVarLog
 	{
 		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogCoroutineTransactions, CsCVarLogCoroutineTransactions);
 		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogCoroutineRunning, CsCVarLogCoroutineRunning);
+		// Scoped Timer
+		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogCoroutinesScopedTimer, CsCVarLogCoroutinesScopedTimer);
 		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogRoutineScopedTimer, CsCVarLogRoutineScopedTimer);
 		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogCoroutineScopedTimer, CsCVarLogCoroutineScopedTimer);
 	}
@@ -56,6 +69,11 @@ namespace NCsCVarLog
 namespace NCsScopedGroup
 {
 	CSCORE_API CS_CREATE_ENUM_STRUCT(EMCsScopedGroup, Coroutine);
+
+	namespace Map
+	{
+		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsScopedGroupMap, Coroutine, CsCVarLogCoroutinesScopedTimer);
+	}
 }
 
 #pragma endregion ScopedGroup
--- CsPlugin/Source/CsCore/Coroutine/CsCVars_Coroutine.h
+++ CsPlugin/Source/CsCore/Coroutine/CsCVars_Coroutine.h
@@ -6,6 +6,7 @@
 extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogCoroutineTransactions;
 extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogCoroutineRunning;
 // Scoped Timer
+extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogCoroutinesScopedTimer;
 extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogRoutineScopedTimer;
 extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogCoroutineScopedTimer;
 
@@ -17,6 +18,7 @@ namespace NCsCVarLog
 	extern CSCORE_API const Type LogCoroutineTransactions;
 	extern CSCORE_API const Type LogCoroutineRunning;
 	// Scoped Timer
+	extern CSCORE_API const Type LogCoroutinesScopedTimer;
 	extern CSCORE_API const Type LogRoutineScopedTimer;
 	extern CSCORE_API const Type LogCoroutineScopedTimer;
 
@@ -25,6 +27,7 @@ namespace NCsCVarLog
 		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogCoroutineTransactions);
 		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogCoroutineRunning);
 		// Scoped Timer
+		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogCoroutinesScopedTimer);
 		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogRoutineScopedTimer);
 		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogCoroutineScopedTimer);
 	}
@@ -38,6 +41,11 @@ namespace NCsCVarLog
 namespace NCsScopedGroup
 {
 	extern CSCORE_API const Type Coroutine;
+
+	namespace Map
+	{
+		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(Coroutine);
+	}
 }
 
 #pragma endregion ScopedGroup

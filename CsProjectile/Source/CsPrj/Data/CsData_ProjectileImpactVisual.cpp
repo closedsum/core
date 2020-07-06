b4c20c0898bf83244d48de323033ce4649a54c5f
closedsum <closedsum@gmail.com>
2020-07-06 14:58:27 -0700

    stubbing in Data for Projectile Impact Visual

---
+++ CsProjectile/Source/CsPrj/Data/CsData_ProjectileImpactVisual.cpp
@@ -0,0 +1,9 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Data/CsData_ProjectileImpactVisual.h"
+#include "CsPrj.h"
+
+const FName ICsData_ProjectileImpactVisual::Name = FName("ICsData_ProjectileImpactVisual");
+
+UCsData_ProjectileImpactVisual::UCsData_ProjectileImpactVisual(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}

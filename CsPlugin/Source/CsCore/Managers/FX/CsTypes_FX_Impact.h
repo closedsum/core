1516b760ac8198318718fb2198d3bfbbdd5efddb
closedsum <closedsum@gmail.com>
2020-07-06 14:58:06 -0700

    Stubbing struct for Impact FX

---
+++ CsPlugin/Source/CsCore/Managers/FX/CsTypes_FX_Impact.h
@@ -0,0 +1,35 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Managers/FX/CsTypes_FX.h"
+#include "Engine/DataTable.h"
+
+#include "CsTypes_FX_Impact.generated.h"
+#pragma once
+
+// FCsFXImpact
+#pragma region
+
+/**
+* Container holding general information for an FX System.
+* and the associated Physics Surface
+*/
+USTRUCT(BlueprintType)
+struct CSCORE_API FCsFXImpact : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TEnumAsByte<EPhysicalSurface> Surface;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FCsFX FX;
+
+public:
+
+	FCsFXImpact() :
+		Surface(EPhysicalSurface::SurfaceType_Default),
+		FX()
+	{
+	}
+};
+
+#pragma endregion FCsFXImpact

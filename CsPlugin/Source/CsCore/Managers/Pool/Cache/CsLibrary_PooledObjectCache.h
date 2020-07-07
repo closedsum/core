9c3b7ba9c3369f183be36f3cd87f0cbe2dcbef21
closedsum <closedsum@gmail.com>
2020-07-07 09:16:05 -0700

    Updated wiht comments and PureStaticCastChecked

--- CsPlugin/Source/CsCore/Managers/Pool/Cache/CsLibrary_PooledObjectCache.h
+++ CsPlugin/Source/CsCore/Managers/Pool/Cache/CsLibrary_PooledObjectCache.h
@@ -6,14 +6,45 @@
 
 struct CSCORE_API FCsLibrary_PooledObjectCache
 {
+	/**
+	* Get the Interface Map from an Cache with checks.
+	* Cache should implement the interface: ICsPooledObjectCache.
+	*
+	* @param Context	The calling context
+	* @param Cache		Cache that implements the interface: ICsPooledObjectCache.
+	* return			Interface Map
+	*/
 	FORCEINLINE static FCsInterfaceMap* GetInterfaceMapChecked(const FString& Context, ICsPooledObjectCache* Cache)
 	{
 		return NCsInterfaceMap::GetInterfaceMapChecked<ICsPooledObjectCache>(Context, Cache);
 	}
 
-	template<typename T>
-	FORCEINLINE static T* StaticCastChecked(const FString& Context, ICsPooledObjectCache* Cache)
+	/**
+	* Perform the operation static_cast<DerivedType*>("Cache associated with ICsPooledObjectCache") with checks.
+	* DerivedType is NOT abstract.
+	*
+	* @param Context	The calling context
+	* @param Cache		Cache that implements the interface: ICsPooledObjectCache.
+	* return			Cache casted to DerivedType (static_cast<DerivedType*>(Cache))
+	*/
+	template<typename DerivedType>
+	FORCEINLINE static DerivedType* StaticCastChecked(const FString& Context, ICsPooledObjectCache* Cache)
 	{
-		return NCsInterfaceMap::StaticCastChecked<T, ICsPooledObjectCache>(Context, Cache);
+		return NCsInterfaceMap::StaticCastChecked<DerivedType, ICsPooledObjectCache>(Context, Cache);
+	}
+
+	/**
+	* Perform the operation static_cast<DerivedType*>("Cache associated with ICsPooledObjectCache") with checks.
+	* DerivedType is NOT abstract.
+	* Does NOT check if the InterfaceMap "emulates" interfaces.
+	*
+	* @param Context	The calling context
+	* @param Cache		Cache that implements the interface: ICsPooledObjectCache.
+	* return			Cache casted to DerivedType (static_cast<DerivedType*>(Cache))
+	*/
+	template<typename DerivedType>
+	FORCEINLINE static DerivedType* PureStaticCastChecked(const FString& Context, ICsPooledObjectCache* Cache)
+	{
+		return NCsInterfaceMap::PureStaticCastChecked<DerivedType, ICsPooledObjectCache>(Context, Cache);
 	}
 };
--- CsPlugin/Source/CsCore/Managers/Pool/Payload/CsLibrary_PooledObjectPayload.h
+++ CsPlugin/Source/CsCore/Managers/Pool/Payload/CsLibrary_PooledObjectPayload.h
@@ -6,17 +6,41 @@
 
 struct CSCORE_API FCsLibrary_PooledObjectPayload
 {
+	/**
+	* Get the Interface Map from an Payload with checks.
+	* Payload should implement the interface: ICsPooledObjectPayload.
+	*
+	* @param Context	The calling context
+	* @param Cache		Payload that implements the interface: ICsPooledObjectPayload.
+	* return			Interface Map
+	*/
 	FORCEINLINE static FCsInterfaceMap* GetInterfaceMapChecked(const FString& Context, ICsPooledObjectPayload* Payload)
 	{
 		return NCsInterfaceMap::GetInterfaceMapChecked<ICsPooledObjectPayload>(Context, Payload);
 	}
 	
+	/**
+	* Perform the operation static_cast<DerivedType*>("Payload associated with ICsPooledObjectPayload") with checks.
+	* DerivedType is NOT abstract.
+	*
+	* @param Context	The calling context
+	* @param Cache		Payload that implements the interface: ICsPooledObjectPayload.
+	* return			Payload casted to DerivedType (static_cast<DerivedType*>(Payload))
+	*/
 	template<typename DerivedType>
 	FORCEINLINE static DerivedType* StaticCastChecked(const FString& Context, ICsPooledObjectPayload* Payload)
 	{
 		return NCsInterfaceMap::StaticCastChecked<DerivedType, ICsPooledObjectPayload>(Context, Payload);
 	}
 
+	/**
+	* Perform the operation static_cast<OtherInterfaceType*>("Payload associated with ICsPooledObjectPayload") with checks.
+	* OtherInterfaceType IS abstract.
+	*
+	* @param Context	The calling context
+	* @param Cache		Payload that implements the interface: ICsPooledObjectPayload.
+	* return			Payload casted to OtherInterfaceType (static_cast<OtherInterfaceType*>(Payload))
+	*/
 	template<typename OtherInterfaceType>
 	FORCEINLINE static OtherInterfaceType* GetInterfaceChecked(const FString& Context, ICsPooledObjectPayload* Payload)
 	{

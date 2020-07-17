2252357121c8e91b1f20fa3d2c0798fa861a75f5
closedsum <closedsum@gmail.com>
2020-07-17 11:32:40 -0700

    Stubbing in Status Effect interface

---
+++ CsStatusEffect/Source/CsSe/CsStatusEffect.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "CsStatusEffect.h"
+#include "CsSe.h"
+
+UCsStatusEffect::UCsStatusEffect(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ CsStatusEffect/Source/CsSe/CsStatusEffect.h
@@ -0,0 +1,18 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+
+#include "UObject/Interface.h"
+#include "CsStatusEffect.generated.h"
+
+UINTERFACE(Blueprintable)
+class CSSE_API UCsStatusEffect : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class CSSE_API ICsStatusEffect
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+};

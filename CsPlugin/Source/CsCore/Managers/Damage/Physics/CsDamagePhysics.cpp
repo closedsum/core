e987f7a17ed88f527a480f29110203d4889708f8
closedsum <closedsum@gmail.com>
2020-07-18 13:21:19 -0700

    Remove Damage Physics will later be encapsulating by some physics / impulse interface

--- CsPlugin/Source/CsCore/Managers/Damage/Physics/CsDamagePhysics.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
-#include "Managers/Damage/Physics/CsDamagePhysics.h"
-
-const FName ICsDamagePhysics::Name = FName("ICsDamagePhysics");

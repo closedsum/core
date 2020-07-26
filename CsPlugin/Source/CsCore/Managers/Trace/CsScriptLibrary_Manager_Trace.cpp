e883fad790ce82defb6e6c26bbdc4469f0d93dbf
closedsum <closedsum@gmail.com>
2020-07-26 16:33:07 -0700

    Script Library

--- CsPlugin/Source/CsCore/Managers/Trace/CsScriptLibrary_Manager_Trace.cpp
+++ CsPlugin/Source/CsCore/Managers/Trace/CsScriptLibrary_Manager_Trace.cpp
@@ -5,8 +5,56 @@
 
 // Managers
 #include "Managers/Trace/CsManager_Trace.h"
+// Actor
+#include "GameFramework/Actor.h"
 
 UCsScriptLibrary_Manager_Trace::UCsScriptLibrary_Manager_Trace(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 }
+
+// Line
+#pragma region
+
+bool UCsScriptLibrary_Manager_Trace::LineTraceSingleByChannel(UObject* WorldContextObject, const FVector Start, const FVector End, const TEnumAsByte<ECollisionChannel>& Channel, bool bTraceComplex, bool bIgnoreSelf, const TArray<AActor*>& ActorsToIgnore, FHitResult& OutHit)
+{
+#if WITH_EDITOR
+	if (UCsManager_Trace* Manager = UCsManager_Trace::GetFromWorldContextObject(WorldContextObject))
+#else
+	if (UCsManager_Trace* Manager = UCsManager_Trace::Get())
+#endif // #if WITH_EDITOR
+	{
+		// Fill out Request
+		FCsTraceRequest* Request = Manager->AllocateRequest();
+		Request->Start				  = Start;
+		Request->End				  = End;
+		Request->Channel			  = Channel;
+		Request->Params.bTraceComplex = bTraceComplex;
+
+		if (bIgnoreSelf)
+		{
+			if (AActor* Actor = Cast<AActor>(WorldContextObject))
+				Request->Params.AddIgnoredActor(Actor);
+		}
+
+		Request->Params.AddIgnoredActors(ActorsToIgnore);
+
+		// Check Response
+		if (FCsTraceResponse* Response = Manager->Trace(Request))
+		{
+			if (Response->bResult)
+			{
+				OutHit = Response->OutHits[CS_FIRST];
+			}
+			return Response->bResult;
+		}
+		return false;
+	}
+	else
+	{
+		UE_LOG(LogCs, Warning, TEXT("UCsScriptLibrary_Manager_Trace::LineTraceSingleByChannel: No Manager Trace of type UCsManager_Trace was created."));
+	}
+	return false;
+}
+
+#pragma endregion Line
--- CsPlugin/Source/CsCore/Managers/Trace/CsScriptLibrary_Manager_Trace.h
+++ CsPlugin/Source/CsCore/Managers/Trace/CsScriptLibrary_Manager_Trace.h
@@ -1,8 +1,10 @@
 // Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
-
 #pragma once
 
 #include "UObject/Object.h"
+// Types
+#include "Engine/EngineTypes.h"
+
 #include "CsScriptLibrary_Manager_Trace.generated.h"
 
 // Delegates
@@ -10,8 +12,29 @@
 
 #pragma endregion Delegates
 
+class AActor;
+
 UCLASS()
 class CSCORE_API UCsScriptLibrary_Manager_Trace : public UObject
 {
 	GENERATED_UCLASS_BODY()
+
+// Line
+#pragma region
+
+	/**
+	* Does a collision trace along the given line and returns the first hit encountered.
+	*
+	* @param WorldContext	World context
+	* @param Start			Start of line segment.
+	* @param End			End of line segment.
+	* @param bTraceComplex	True to test against complex collision, false to test against simplified collision.
+	* @param Channel		
+	* @param OutHit			Properties of the trace hit.
+	* @return				True if there was a hit, false otherwise.
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Library|Manager|Trace", meta = (bIgnoreSelf = "true", WorldContext = "WorldContextObject", AutoCreateRefTerm = "ActorsToIgnore", DisplayName = "LineTraceSingleByChannel"))
+	static bool LineTraceSingleByChannel(UObject* WorldContextObject, const FVector Start, const FVector End, const TEnumAsByte<ECollisionChannel>& Channel, bool bTraceComplex, bool bIgnoreSelf, const TArray<AActor*>& ActorsToIgnore, FHitResult& OutHit);
+
+#pragma endregion Line
 };

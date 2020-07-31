bebdaead557973b049be852ab276c255a3dff444
closedsum <closedsum@gmail.com>
2020-07-31 10:36:55 -0700

    Stubbing in version of streamable handle that also includes an Id - this is for bookkeeping and to probably "unload" DataTable and / or its row(s)

---
+++ CsPlugin/Source/CsCore/Managers/Load/CsTypes_Streaming.h
@@ -0,0 +1,59 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+#include "Engine/StreamableManager.h"
+
+// FCsStreamableHandle
+#pragma region
+
+struct CSCORE_API FCsStreamableHandle
+{
+public:
+
+	//static const FCsStreamableHandle Invalid;
+
+public:
+
+	FGuid Id;
+
+	TSharedPtr<FStreamableHandle> Handle;
+
+	FCsStreamableHandle() :
+		Id(),
+		Handle(nullptr)
+	{
+	}
+
+	FORCEINLINE bool operator==(const FCsStreamableHandle& B) const
+	{
+		return Id == B.Id;
+	}
+
+	FORCEINLINE bool operator!=(const FCsStreamableHandle& B) const
+	{
+		return !(*this == B);
+	}
+
+	friend uint32 GetTypeHash(const FCsStreamableHandle& InHandle)
+	{
+		return GetTypeHash(InHandle.Id);
+	}
+
+	FORCEINLINE bool IsValid() const
+	{
+		return Id.IsValid() && Handle.IsValid();
+	}
+
+	FORCEINLINE void New()
+	{
+		Id.NewGuid();
+	}
+
+	FORCEINLINE void Reset()
+	{
+		Id.Invalidate();
+
+		Handle = nullptr;
+	}
+};
+
+#pragma endregion FCsStreamableHandle

5068a81f37c55719781b0742d746885fe5953b71
closedsum <closedsum@gmail.com>
2020-08-24 16:58:48 -0700

    Adding InputCore

--- CsUserInterface/Source/CsUI/CsUI.Build.cs
+++ CsUserInterface/Source/CsUI/CsUI.Build.cs
@@ -18,5 +18,11 @@ public class CsUI : ModuleRules
                     "UMG",
                 }
             );
+
+       PrivateDependencyModuleNames.AddRange(
+                new string[] {
+                    "InputCore",
+                }
+            );
     }
 }

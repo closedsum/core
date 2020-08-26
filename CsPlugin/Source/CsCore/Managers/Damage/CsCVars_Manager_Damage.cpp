b12e2cabad008177d479d293255d799afd9f2e28
closedsum <closedsum@gmail.com>
2020-08-25 18:16:31 -0700

    Stubbing cvars for different transactions

--- CsPlugin/Source/CsCore/Managers/Damage/CsCVars_Manager_Damage.cpp
+++ CsPlugin/Source/CsCore/Managers/Damage/CsCVars_Manager_Damage.cpp
@@ -8,16 +8,43 @@ TAutoConsoleVariable<int32> CsCVarLogManagerDamageEvents(
 	ECVF_SetByConsole
 );
 
+TAutoConsoleVariable<int32> CsCVarLogManagerDamageTransactionEvent(
+	TEXT("log.manager.damage.transaction.event"),
+	0,
+	TEXT("Log Manager Damage Transaction Event."),
+	ECVF_SetByConsole
+);
+
+TAutoConsoleVariable<int32> CsCVarLogManagerDamageTransactionValue(
+	TEXT("log.manager.damage.transaction.value"),
+	0,
+	TEXT("Log Manager Damage Transaction Value."),
+	ECVF_SetByConsole
+);
+
+TAutoConsoleVariable<int32> CsCVarLogManagerDamageTransactionRange(
+	TEXT("log.manager.damage.transaction.range"),
+	0,
+	TEXT("Log Manager Damage Transaction Range."),
+	ECVF_SetByConsole
+);
+
 // CVarLog
 #pragma region
 
 namespace NCsCVarLog
 {
 	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogManagerDamageEvents, "Log Manager Damage Events");
+	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogManagerDamageTransactionEvent, "Log Manager Damage Transaction Event");
+	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogManagerDamageTransactionEvent, "Log Manager Damage Transaction Value");
+	CSCORE_API CS_CREATE_ENUM_STRUCT_CUSTOM(EMCsCVarLog, LogManagerDamageTransactionEvent, "Log Manager Damage Transaction Range");
 
 	namespace Map
 	{
 		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogManagerDamageEvents, CsCVarLogManagerDamageEvents);
+		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogManagerDamageTransactionEvent, CsCVarLogManagerDamageTransactionEvent);
+		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogManagerDamageTransactionValue, CsCVarLogManagerDamageTransactionValue);
+		CSCORE_API CS_ADD_TO_CVAR_MAP(FCsCVarLogMap, LogManagerDamageTransactionRange, CsCVarLogManagerDamageTransactionRange);
 	}
 }
 
--- CsPlugin/Source/CsCore/Managers/Damage/CsCVars_Manager_Damage.h
+++ CsPlugin/Source/CsCore/Managers/Damage/CsCVars_Manager_Damage.h
@@ -4,6 +4,9 @@
 #pragma once
 
 extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogManagerDamageEvents;
+extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogManagerTransactionEvent;
+extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogManagerTransactionValue;
+extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogManagerTransactionRange;
 
 // CVarLog
 #pragma region
@@ -11,10 +14,16 @@ extern CSCORE_API TAutoConsoleVariable<int32> CsCVarLogManagerDamageEvents;
 namespace NCsCVarLog
 {
 	extern CSCORE_API const Type LogManagerDamageEvents;
+	extern CSCORE_API const Type LogManagerDamageTransactionEvent;
+	extern CSCORE_API const Type LogManagerDamageTransactionValue;
+	extern CSCORE_API const Type LogManagerDamageTransactionRange;
 
 	namespace Map
 	{
 		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogManagerDamageEvents);
+		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogManagerDamageTransactionEvent);
+		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogManagerDamageTransactionValue);
+		extern CSCORE_API CS_DECLARE_ADD_TO_CVAR_MAP(LogManagerDamageTransactionRange);
 	}
 }
 

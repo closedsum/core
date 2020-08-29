49577e391ce3cf1a37b5055d5453ac34aadb5f83
closedsum <closedsum@gmail.com>
2020-08-28 17:24:44 -0700

    Adding Utility class for Populating Enum Struct Maps

---
+++ CsPlugin/Source/CsCore/Utility/CsPopulateEnumMapFromSettings.cpp
@@ -0,0 +1,5 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Utility/CsPopulateEnumMapFromSettings.h"
+
+const FName FCsPopulateEnumMapFromSettings::NamePropertyName = FName("Name");
+const FName FCsPopulateEnumMapFromSettings::DisplayNamePropertyName = FName("DisplayName");

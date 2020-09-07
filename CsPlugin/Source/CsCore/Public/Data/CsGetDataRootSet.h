119a142e031a450f0fdad1f3d88659121bbc4e93
closedsum <closedsum@gmail.com>
2020-09-07 15:33:46 -0700

    Stubbing in get for core's DataRootSet

---
+++ CsPlugin/Source/CsCore/Public/Data/CsGetDataRootSet.h
@@ -0,0 +1,27 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+#include "UObject/Interface.h"
+// Types
+//#include "Data/CsDataRootSet.h"
+
+#include "CsGetDataRootSet.generated.h"
+
+UINTERFACE(BlueprintType)
+class CSCORE_API UCsGetDataRootSet : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class CSCORE_API ICsGetDataRootSet
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	/**
+	*
+	*
+	* return
+	*/
+	//virtual const FCsDataRootSet& GetCsDataRootSet() const = 0;
+};

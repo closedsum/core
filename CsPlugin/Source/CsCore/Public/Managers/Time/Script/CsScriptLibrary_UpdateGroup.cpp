6f13993b79a4cef8e5e7d2469894e1f1c7a985fc
Chetan <beditheory@gmail.com>
2021-05-06 17:27:04 -0700

    Making function signatures shorter

--- CsPlugin/Source/CsCore/Public/Managers/Time/Script/CsScriptLibrary_UpdateGroup.cpp
+++ CsPlugin/Source/CsCore/Public/Managers/Time/Script/CsScriptLibrary_UpdateGroup.cpp
@@ -15,8 +15,8 @@ namespace NCsScriptLibraryUpdateGroup
 		namespace Str
 		{
 			const FString FECsUpdateGroup = TEXT("FECsUpdateGroup");
-			const FString ECsUpdateGroup_Get = TEXT("ECsUpdateGroup_Get");
-			const FString ECsUpdateGroup_GetByIndex = TEXT("ECsUpdateGroup_GetByIndex");
+			const FString Get = TEXT("Get");
+			const FString GetByIndex = TEXT("GetByIndex");
 		}
 	}
 }
@@ -29,51 +29,51 @@ UCsScriptLibrary_UpdateGroup::UCsScriptLibrary_UpdateGroup(const FObjectInitiali
 }
 
 
-FECsUpdateGroup UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_Get(const FString& Name)
+FECsUpdateGroup UCsScriptLibrary_UpdateGroup::Get(const FString& Name)
 {
 	using namespace NCsScriptLibraryUpdateGroup::NCached;
 
-	const FString& Context = Str::ECsUpdateGroup_Get;
+	const FString& Context = Str::Get;
 
 	typedef NCsEnum::FLibrary EnumLibrary;
 
 	return EnumLibrary::GetSafe<EMCsUpdateGroup, FECsUpdateGroup>(Context, Str::FECsUpdateGroup, Name);
 }
 
-FECsUpdateGroup UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_GetByIndex(const int32& Index)
+FECsUpdateGroup UCsScriptLibrary_UpdateGroup::GetByIndex(const int32& Index)
 {
 	using namespace NCsScriptLibraryUpdateGroup::NCached;
 
-	const FString& Context = Str::ECsUpdateGroup_GetByIndex;
+	const FString& Context = Str::GetByIndex;
 
 	typedef NCsEnum::FLibrary EnumLibrary;
 
 	return EnumLibrary::GetSafeByIndex<EMCsUpdateGroup, FECsUpdateGroup>(Context, Str::FECsUpdateGroup, Index);
 }
 
-FString UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_ToString(const FECsUpdateGroup& Enum)
+FString UCsScriptLibrary_UpdateGroup::ToString(const FECsUpdateGroup& Enum)
 {
 	return Enum.ToString();
 }
 
-uint8 UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_GetCount()
+uint8 UCsScriptLibrary_UpdateGroup::GetCount()
 {
 	return EMCsUpdateGroup::Get().Num();
 }
 
-void UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_GetAll(TArray<FECsUpdateGroup>& OutTypes)
+void UCsScriptLibrary_UpdateGroup::GetAll(TArray<FECsUpdateGroup>& OutTypes)
 {
 	typedef NCsEnum::FLibrary EnumLibrary;
 
 	EnumLibrary::GetAll<EMCsUpdateGroup, FECsUpdateGroup>(OutTypes);
 }
 
-FECsUpdateGroup UCsScriptLibrary_UpdateGroup::ECsUpdateGroup_GetMax()
+FECsUpdateGroup UCsScriptLibrary_UpdateGroup::GetMax()
 {
 	return EMCsUpdateGroup::Get().GetMAX();
 }
 
-bool UCsScriptLibrary_UpdateGroup::EqualEqual_UpdateGroupUpdateGroup(const FECsUpdateGroup& A, const FECsUpdateGroup& B)
+bool UCsScriptLibrary_UpdateGroup::EqualEqual(const FECsUpdateGroup& A, const FECsUpdateGroup& B)
 {
 	return A == B;
 }
--- CsPlugin/Source/CsCore/Public/Managers/Time/Script/CsScriptLibrary_UpdateGroup.h
+++ CsPlugin/Source/CsCore/Public/Managers/Time/Script/CsScriptLibrary_UpdateGroup.h
@@ -14,24 +14,24 @@ class CSCORE_API UCsScriptLibrary_UpdateGroup : public UObject
 
 public:
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - Get", AutoCreateRefTerm = "Name"))
-	static FECsUpdateGroup ECsUpdateGroup_Get(const FString& Name);
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (AutoCreateRefTerm = "Name"))
+	static FECsUpdateGroup Get(const FString& Name);
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - Get by Index", AutoCreateRefTerm = "Index"))
-	static FECsUpdateGroup ECsUpdateGroup_GetByIndex(const int32& Index);
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (AutoCreateRefTerm = "Index"))
+	static FECsUpdateGroup GetByIndex(const int32& Index);
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - To String", AutoCreateRefTerm = "Enum"))
-	static FString ECsUpdateGroup_ToString(const FECsUpdateGroup& Enum);
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (AutoCreateRefTerm = "Enum"))
+	static FString ToString(const FECsUpdateGroup& Enum);
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - Get Count"))
-	static uint8 ECsUpdateGroup_GetCount();
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup")
+	static uint8 GetCount();
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - Get All"))
-	static void ECsUpdateGroup_GetAll(TArray<FECsUpdateGroup>& OutTypes);
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup")
+	static void GetAll(TArray<FECsUpdateGroup>& OutTypes);
 
-	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (DisplayName = "ECsUpdateGroup - Get Max", AutoCreateRefTerm = "Name"))
-	static FECsUpdateGroup ECsUpdateGroup_GetMax();
+	UFUNCTION(BlueprintPure, Category = "CsCore|Library|UpdateGroup", meta = (AutoCreateRefTerm = "Name"))
+	static FECsUpdateGroup GetMax();
 
 	UFUNCTION(BlueprintPure, meta = (DisplayName = "Equal (UpdateGroup)", CompactNodeTitle = "==", ScriptMethod = "Equals", Keywords = "== equal"), Category = "CsCore|Library|UpdateGroup")
-	static bool EqualEqual_UpdateGroupUpdateGroup(const FECsUpdateGroup& A, const FECsUpdateGroup& B);
+	static bool EqualEqual(const FECsUpdateGroup& A, const FECsUpdateGroup& B);
 };

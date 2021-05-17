8ab453ef61589e02c28e6e9fde37f993e1332e48
Chetan <beditheory@gmail.com>
2021-05-17 11:02:37 -0700

    Updated for non unity build

--- CsPlatformServices/Source/CsPlatformServices/CsPlatformServices.Build.cs
+++ CsPlatformServices/Source/CsPlatformServices/CsPlatformServices.Build.cs
@@ -8,6 +8,7 @@ public class CsPlatformServices : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         bLegacyPublicIncludePaths = false;
+        bUseUnity = System.Environment.ProcessorCount < 64;
 
         PublicDependencyModuleNames.AddRange(
                 new string[] {

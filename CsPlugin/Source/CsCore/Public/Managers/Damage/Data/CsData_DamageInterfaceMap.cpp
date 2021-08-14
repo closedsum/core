63f6c7ef3109d0d44418346074d1fe14d9c1d777
closedsum <closedsum@gmail.com>
2021-08-13 18:16:46 -0700

    STubbing in interface map for damage data

---
+++ CsPlugin/Source/CsCore/Public/Managers/Damage/Data/CsData_DamageInterfaceMap.cpp
@@ -0,0 +1,47 @@
+// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+#include "Managers/Damage/Data/CsData_DamageInterfaceMap.h"
+
+#include "Containers/CsInterfaceMap.h"
+
+const FName NCsDamage::NData::FInterfaceMap::Name = FName("NCsDamage::NData::FInterfaceMap");
+
+namespace NCsDamage
+{
+	namespace NData
+	{
+		FInterfaceMap::FInterfaceMap() :
+			// ICsGetInterfaceMap
+			InterfaceMap(nullptr),
+			// ICsDeconstructInterfaceSliceMap
+			SliceByNameMap(),
+			DeconstructSliceImplByNameMap()
+
+		{
+			// ICsGetInterfaceMap
+			InterfaceMap = new FCsInterfaceMap();
+
+			InterfaceMap->SetRoot<FInterfaceMap>(this);
+			InterfaceMap->SetUniqueBasedSlices();
+		}
+
+		FInterfaceMap::~FInterfaceMap()
+		{
+			for (TPair<FName, void*>& Pair : SliceByNameMap)
+			{
+				const FName& SliceName = Pair.Key;
+				void* Slice			   = Pair.Value;
+
+				void(*DeconstructImpl)(void*) = DeconstructSliceImplByNameMap[SliceName];
+
+				DeconstructImpl(Slice);
+
+				Pair.Value = nullptr;
+			}
+			SliceByNameMap.Reset();
+			DeconstructSliceImplByNameMap.Reset();
+
+			// ICsGetInterfaceMap
+			delete InterfaceMap;
+		}
+	}
+}

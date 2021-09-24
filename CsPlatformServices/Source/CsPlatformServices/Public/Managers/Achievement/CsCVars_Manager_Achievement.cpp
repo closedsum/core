23203b5544a19ad07ac0be824c8816034c56981a
closedsum <closedsum@gmail.com>
2021-09-24 15:07:10 -0700

    update copyright

--- CsPlatformServices/Source/CsPlatformServices/Public/Managers/Achievement/CsCVars_Manager_Achievement.cpp
+++ CsPlatformServices/Source/CsPlatformServices/Public/Managers/Achievement/CsCVars_Manager_Achievement.cpp
@@ -1,4 +1,4 @@
-// Copyright 2017-2019 Closed Sum Games, LLC. All Rights Reserved.
+// Copyright 2017-2021 Closed Sum Games, LLC. All Rights Reserved.
 #include "CsCVars_Manager_Achievement.h"
 
 TAutoConsoleVariable<int32> CsCVarLogManagerAchievementTransactions(

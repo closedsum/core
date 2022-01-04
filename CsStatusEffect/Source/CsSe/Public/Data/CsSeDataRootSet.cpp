c85ef15bfe48c5dc9ad12cc355a7c7c5894d90b6
closedsum <closedsum@gmail.com>
2022-01-04 12:55:54 -0800

    Add more info for Status Effect DataRootSet

---
+++ CsStatusEffect/Source/CsSe/Public/Data/CsSeDataRootSet.cpp
@@ -0,0 +1,104 @@
+// Copyright 2017-2021 Closed Sum Games, LLC. All Rights Reserved.
+#include "Data/CsSeDataRootSet.h"
+#include "CsSe.h"
+
+// Types
+#include "Types/CsTypes_Macro.h"
+// Library
+#include "Data/CsLibrary_DataRootSet.h"
+// DataTable
+#include "Engine/DataTable.h"
+
+// Cached
+#pragma region
+
+namespace NCsSeDataRootSet
+{
+	namespace NCached
+	{
+		namespace Str
+		{
+			CS_DEFINE_CACHED_STRING(GetCsSeDataRootSet, "GetCsSeDataRootSet()");
+
+			CS_DEFINE_CACHED_STRING(StatusEffects, "StatusEffects");
+		}
+	}
+}
+
+#pragma endregion Cached
+
+bool FCsSeDataRootSet::IsValidChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const
+{
+	using namespace NCsSeDataRootSet::NCached;
+
+	#define CS_TEMP_CHECK(Member) if (MemberType == EMember::Member) \
+	{ \
+		checkf(Member.ToSoftObjectPath().IsValid(), TEXT("%s: %s.%s.%s is NOT Valid."), *Context, *(WorldContext->GetName(), *Str::GetCsSeDataRootSet, *Str::Member)); \
+	}
+
+	// StatusEffects
+	CS_TEMP_CHECK(StatusEffects)
+
+	#undef CS_TEMP_CHECK
+
+	return true;
+}
+
+const TSoftObjectPtr<UDataTable>& FCsSeDataRootSet::GetDataTableSoftObjectChecked(const FString& Context, const EMember& MemberType) const
+{
+	#define CS_TEMP_GET_DATA_TABLE_SOFT_OBJECT_CHECKED(Member) if (MemberType == EMember::Member) \
+		return Member;
+
+	// StatusEffects
+	CS_TEMP_GET_DATA_TABLE_SOFT_OBJECT_CHECKED(StatusEffects)
+
+	#undef CS_TEMP_GET_DATA_TABLE_SOFT_OBJECT_CHECKED
+
+	checkf(0, TEXT("%s: Failed to get DataTable SoftObject for MemberType."), *Context);
+	return StatusEffects;
+}
+
+UDataTable* FCsSeDataRootSet::GetSafeDataTable(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const
+{
+	using namespace NCsSeDataRootSet::NCached;
+
+	typedef NCsDataRootSet::FLibrary DataRootSetLibrary;
+
+	#define CS_TEMP_GET_SAFE_DATA_TABLE(Member) if (MemberType == EMember::Member) \
+		return DataRootSetLibrary::GetSafeDataTable(Context, WorldContext, Str::GetCsSeDataRootSet, Member, Str::Member);
+
+	// StatusEffects
+	CS_TEMP_GET_SAFE_DATA_TABLE(StatusEffects)
+
+	#undef CS_TEMP_GET_SAFE_DATA_TABLE
+
+	UE_LOG(LogCsSe, Warning, TEXT("%s: Failed to get DataTable for MemberType."), *Context);
+	return nullptr;
+}
+
+UDataTable* FCsSeDataRootSet::GetDataTableChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const
+{
+	using namespace NCsSeDataRootSet::NCached;
+
+	typedef NCsDataRootSet::FLibrary DataRootSetLibrary;
+
+	return DataRootSetLibrary::GetDataTableChecked(Context, WorldContext, GetDataTableSoftObjectChecked(Context, MemberType));
+}
+
+uint8* FCsSeDataRootSet::GetDataTableRowChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType, const FName& RowName) const
+{
+	using namespace NCsSeDataRootSet::NCached;
+
+	typedef NCsDataRootSet::FLibrary DataRootSetLibrary;
+
+	return DataRootSetLibrary::GetDataTableRowChecked(Context, WorldContext, GetDataTableSoftObjectChecked(Context, MemberType), RowName);
+}
+
+uint8* FCsSeDataRootSet::GetDataTableRowChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType, const UScriptStruct* RowStruct, const FName& RowName) const
+{
+	using namespace NCsSeDataRootSet::NCached;
+
+	typedef NCsDataRootSet::FLibrary DataRootSetLibrary;
+
+	return DataRootSetLibrary::GetDataTableRowChecked(Context, WorldContext, GetDataTableSoftObjectChecked(Context, MemberType), RowStruct, RowName);
+}
--- CsStatusEffect/Source/CsSe/Public/Data/CsSeDataRootSet.h
+++ CsStatusEffect/Source/CsSe/Public/Data/CsSeDataRootSet.h
@@ -1,10 +1,11 @@
 // Copyright 2017-2021 Closed Sum Games, LLC. All Rights Reserved.
 #pragma once
-//#include "Settings/CsTypes_ProjectileSettings.h"
+#include "Engine/DataTable.h"
 
 #include "CsSeDataRootSet.generated.h"
 
-class UDataTable;
+class UObject;
+class UScriptStruct;
 
 USTRUCT(BlueprintType)
 struct CSSE_API FCsSeDataRootSet
@@ -13,7 +14,49 @@ struct CSSE_API FCsSeDataRootSet
 
 public:
 
-	FCsSeDataRootSet()
+	/** Status Effect Datas */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSoftObjectPtr<UDataTable> StatusEffects;
+
+	FCsSeDataRootSet() :
+		StatusEffects()
 	{
 	}
+
+	enum class EMember : uint8
+	{
+		StatusEffects
+	};
+
+		bool IsValidChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const;
+
+	const TSoftObjectPtr<UDataTable>& GetDataTableSoftObjectChecked(const FString& Context, const EMember& MemberType) const;
+
+	UDataTable* GetSafeDataTable(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const;
+
+	UDataTable* GetDataTableChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType) const;
+
+	template<typename RowStructType>
+	RowStructType* GetSafeDataTableRow(const FString& Context, const UObject* WorldContext, const EMember& MemberType, const FName& RowName, void(*Log)(const FString&)) const
+	{
+		if (UDataTable* DataTable = GetSafeDataTable(Context, WorldContext, MemberType))
+		{
+			if (RowStructType* RowPtr = DataTable->FindRow<RowStructType>(RowName, Context))
+			{
+				return RowPtr;
+			}
+			else
+			{
+#if !UE_BUILD_SHIPPING
+				if (Log)
+					Log(FString::Printf(TEXT("%s: Failed to find Row: %s from DataTable: %s."), *Context, *(RowName.ToString()), *(DataTable->GetName())));
+#endif // #if !UE_BUILD_SHIPPING
+			}
+		}
+		return nullptr;
+	}
+
+	uint8* GetDataTableRowChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType, const FName& RowName) const;
+
+	uint8* GetDataTableRowChecked(const FString& Context, const UObject* WorldContext, const EMember& MemberType, const UScriptStruct* RowStruct, const FName& RowName) const;
 };

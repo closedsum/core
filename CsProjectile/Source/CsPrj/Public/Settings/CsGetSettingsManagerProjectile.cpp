918991a23c8e099c9f89a4be944428c30c2fb6f6
closedsum <closedsum@gmail.com>
2022-01-23 19:01:31 -0800

    Adding another interface for getting Manager Projectile settings

---
+++ CsProjectile/Source/CsPrj/Public/Settings/CsGetSettingsManagerProjectile.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Settings/CsGetSettingsManagerProjectile.h"
+#include "CsPrj.h"
+
+UCsGetSettingsManagerProjectile::UCsGetSettingsManagerProjectile(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ CsProjectile/Source/CsPrj/Public/Settings/CsGetSettingsManagerProjectile.h
@@ -0,0 +1,25 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+// Interfaces
+#include "UObject/Interface.h"
+// Types
+#include "Managers/Projectile/CsSettings_Manager_Projectile.h"
+
+#include "CsGetSettingsManagerProjectile.generated.h"
+
+UINTERFACE(Blueprintable)
+class CSPRJ_API UCsGetSettingsManagerProjectile : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+class CSPRJ_API ICsGetSettingsManagerProjectile
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	/**
+	*/
+	virtual const FCsSettings_Manager_Projectile& GetSettingsManagerProjectile() const = 0;
+};

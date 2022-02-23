c7afbb71a2f17760eaf1cd7a4a10ee34dc623767
closedsum <closedsum@gmail.com>
2022-02-23 13:04:21 -0800

    Stubbing in Uniform Range Scale skin

---
+++ CsPlugin/Source/CsCore/Public/Skin/Data/Visual/Scale/CsData_Skin_VisualScale_UniformRange.cpp
@@ -0,0 +1,11 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Skin/Data/Visual/Scale/CsData_Skin_VisualScale_UniformRange.h"
+#include "CsCore.h"
+
+const FName NCsSkin::NData::NVisual::NScale::NUniform::NRange::IRange::Name = FName("NCsSkin::NData::NVisual::NScale::NUniform::NRange::IRange");
+
+const FName ICsData_Skin_VisualScale_UniformRange::Name = FName("ICsData_Skin_VisualScale_UniformRange");
+
+UCsData_Skin_VisualScale_UniformRange::UCsData_Skin_VisualScale_UniformRange(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}

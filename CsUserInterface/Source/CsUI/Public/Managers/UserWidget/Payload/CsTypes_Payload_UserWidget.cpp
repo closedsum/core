5cdaa763bd01339bd4b57fb32c837f64f370cad1
closedsum <closedsum@gmail.com>
2022-04-06 10:38:53 -0700

    Stubbing in UserWidgetPayload

---
+++ CsUserInterface/Source/CsUI/Public/Managers/UserWidget/Payload/CsTypes_Payload_UserWidget.cpp
@@ -0,0 +1,14 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Managers/UserWidget/Payload/CsTypes_Payload_UserWidget.h"
+#include "CsUI.h"
+
+// UserWidgetPayload
+#pragma region
+
+namespace NCsUserWidgetPayload
+{
+	CSUI_API CS_CREATE_ENUM_STRUCT_CUSTOM(UserWidget, "NCsUserWidget::NPayload::IPayload");
+	CSUI_API CS_CREATE_ENUM_STRUCT_CUSTOM(Text, "NCsUserWidget::NPayload::NText::IText");
+}
+
+#pragma endregion UserWidgetPayload
---
+++ CsUserInterface/Source/CsUI/Public/Managers/UserWidget/Payload/CsTypes_Payload_UserWidget.h
@@ -0,0 +1,37 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+#include "Types/Enum/CsEnum_uint8.h"
+#include "Types/Enum/CsEnumStructMap.h"
+
+#include "CsTypes_Payload_UserWidget.generated.h"
+
+// UserWidgetPayload
+#pragma region
+
+/**
+*/
+USTRUCT(BlueprintType)
+struct CSUI_API FECsUserWidgetPayload : public FECsEnum_uint8
+{
+	GENERATED_USTRUCT_BODY()
+
+	CS_ENUM_UINT8_BODY(FECsUserWidgetPayload)
+};
+
+CS_DEFINE_ENUM_UINT8_GET_TYPE_HASH(FECsUserWidgetPayload)
+
+struct CSUI_API EMCsUserWidgetPayload : public TCsEnumStructMap<FECsUserWidgetPayload, uint8>
+{
+	CS_ENUM_STRUCT_MAP_BODY(EMCsUserWidgetPayload, FECsUserWidgetPayload, uint8)
+};
+
+namespace NCsUserWidgetPayload
+{
+	typedef FECsUserWidgetPayload Type;
+	typedef EMCsUserWidgetPayload EnumMapType;
+
+	extern CSUI_API const Type UserWidget;
+	extern CSUI_API const Type Text;
+}
+
+#pragma endregion UserWidgetPayload

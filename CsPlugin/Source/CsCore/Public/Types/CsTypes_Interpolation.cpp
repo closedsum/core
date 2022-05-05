0f98222c06f5e8a06eb9140c9061ee5cb8db6224
closedsum <closedsum@gmail.com>
2022-05-05 16:56:48 -0700

    Adding struct for easing a float

--- CsPlugin/Source/CsCore/Public/Types/CsTypes_Interpolation.cpp
+++ CsPlugin/Source/CsCore/Public/Types/CsTypes_Interpolation.cpp
@@ -1,6 +1,10 @@
 // Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
 #include "Types/CsTypes_Interpolation.h"
 
+// Library
+#include "Library/CsLibrary_Math.h"
+#include "Library/CsLibrary_Valid.h"
+
 // BlendDirection
 #pragma region
 
@@ -60,3 +64,27 @@ namespace NCsInterpolatingMethod
 }
 
 #pragma endregion InterpolatingMethod
+
+namespace NCsEasing
+{
+	bool FFloat::IsValidChecked(const FString& Context) const
+	{
+		CS_IS_ENUM_VALID_CHECKED(EMCsEasingType, GetType());
+		CS_IS_FLOAT_GREATER_THAN_OR_EQUAL_CHECKED(GetMax(), GetMin())
+		return true;
+	}
+
+	bool FFloat::IsValid(const FString& Context, void(*Log)(const FString&) /*=&FCsLog::Warning*/) const
+	{
+		CS_IS_ENUM_VALID(EMCsEasingType, ECsEasingType, GetType())
+		CS_IS_FLOAT_GREATER_THAN_OR_EQUAL(GetMax(), GetMin())
+		return true;
+	}
+
+	float FFloat::Ease(const float& Alpha) const
+	{
+		typedef NCsMath::FLibrary MathLibrary;
+
+		return MathLibrary::Ease(GetType(), Alpha, GetMin(), GetMax(), 1.0f);
+	}
+}
--- CsPlugin/Source/CsCore/Public/Types/CsTypes_Interpolation.h
+++ CsPlugin/Source/CsCore/Public/Types/CsTypes_Interpolation.h
@@ -1,6 +1,8 @@
 // Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
 #pragma once
 #include "Types/Enum/CsEnumMap.h"
+// Log
+#include "Utility/CsLog.h"
 
 //#include "CsTypes_Interpolation.generated.h"
 
@@ -123,3 +125,36 @@ namespace NCsInterpolatingMethod
 }
 
 #pragma endregion InterpolatingMethod
+
+namespace NCsEasing
+{
+	struct CSCORE_API FFloat
+	{
+	private:
+
+		CS_DECLARE_MEMBER_WITH_PROXY(Type, ECsEasingType)
+		CS_DECLARE_MEMBER_WITH_PROXY(Min, float)
+		CS_DECLARE_MEMBER_WITH_PROXY(Max, float)
+
+	public:
+
+		FFloat() :
+			CS_CTOR_INIT_MEMBER_WITH_PROXY(Type, ECsEasingType::Linear),
+			CS_CTOR_INIT_MEMBER_WITH_PROXY(Min, 0.0f),
+			CS_CTOR_INIT_MEMBER_WITH_PROXY(Max, 0.0f)
+		{
+			CS_CTOR_SET_MEMBER_PROXY(Type);
+			CS_CTOR_SET_MEMBER_PROXY(Min);
+			CS_CTOR_SET_MEMBER_PROXY(Max);
+		}
+
+		CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(Type, ECsEasingType)
+		CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(Min, float)
+		CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(Max, float)
+
+		bool IsValidChecked(const FString& Context) const;
+		bool IsValid(const FString& Context, void(*Log)(const FString&) = &FCsLog::Warning) const;
+
+		float Ease(const float& Alpha) const;
+	};
+}

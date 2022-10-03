92f20dc3ab72b8f481156eb3f7404a4271160e78
closedsum <closedsum@gmail.com>
2022-10-03 15:27:27 -0700

    Adding interface to Get Damage Modifiers from an Object

---
+++ CsDamage/Source/CsDmg/Public/Modifier/CsGetDamageModifiers.cpp
@@ -0,0 +1,7 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Modifier/CsGetDamageModifiers.h"
+#include "CsDmg.h"
+
+UCsGetDamageModifiers::UCsGetDamageModifiers(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ CsDamage/Source/CsDmg/Public/Modifier/CsGetDamageModifiers.h
@@ -0,0 +1,30 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+// Interfaces
+#include "UObject/Interface.h"
+// Types
+#include "Types/CsTypes_Macro.h"
+
+#include "CsGetDamageModifiers.generated.h"
+
+UINTERFACE(Blueprintable)
+class CSDMG_API UCsGetDamageModifiers : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+// NCsDamage::NModifier::IModifier
+CS_FWD_DECLARE_STRUCT_NAMESPACE_2(NCsDamage, NModifier, IModifier)
+
+class CSDMG_API ICsGetDamageModifiers
+{
+	GENERATED_IINTERFACE_BODY()
+
+#define DmgModifierType NCsDamage::NModifier::IModifier
+
+public:
+
+	virtual const TArray<DmgModifierType*>& GetDamageModifiers() const = 0;
+
+#undef DmgModifierType
+};

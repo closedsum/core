3f8cdfe697929f9ca6878f69b5154223c98d8877
closedsum <closedsum@gmail.com>
2022-11-26 16:39:56 -0800

    Stubbing in interface to get Material Parameter Collection from Skin

---
+++ CsPlugin/Source/CsCore/Public/Skin/Data/Visual/Material/Parameter/Collection/CsData_Skin_VisualMaterial_ParameterCollection.cpp
@@ -0,0 +1,11 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Skin/Data/Visual/Material/Parameter/Collection/CsData_Skin_VisualMaterial_ParameterCollection.h"
+#include "CsCore.h"
+
+const FName NCsSkin::NData::NVisual::NMaterial::NParameter::NCollection::ICollection::Name = FName("NCsSkin::NData::NVisual::NMaterial::NParameter::NCollection::ICollection");
+
+const FName ICsData_Skin_VisualMaterial_ParameterCollection::Name = FName("ICsData_Skin_VisualMaterial_ParameterCollection");
+
+UCsData_Skin_VisualMaterial_ParameterCollection::UCsData_Skin_VisualMaterial_ParameterCollection(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}

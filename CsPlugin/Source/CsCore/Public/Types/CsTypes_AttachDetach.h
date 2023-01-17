eb7c1dc47c2d79a61d09b647ed0dabec81704830
closedsum <closedsum@gmail.com>
2023-01-16 16:01:42 -0800

    removing enum

--- CsPlugin/Source/CsCore/Public/Types/CsTypes_AttachDetach.h
+++ CsPlugin/Source/CsCore/Public/Types/CsTypes_AttachDetach.h
@@ -145,53 +145,6 @@ struct CSCORE_API FCsAttachmentTransformRules
 
 #pragma endregion FCsAttachmentTransformRules
 
-// AttachmentTransformRules
-#pragma region
-
-UENUM(BlueprintType)
-enum class ECsAttachmentTransformRules : uint8
-{
-	KeepRelativeTransform			UMETA(DisplayName = "Keep Relative Transform"),
-	KeepWorldTransform				UMETA(DisplayName = "Keep World Transform"),
-	SnapToTargetNotIncludingScale	UMETA(DisplayName = "Snap To Target Not Including Scale"),
-	SnapToTargetIncludingScale		UMETA(DisplayName = "Snap To Target Including Scale"),
-	ECsAttachmentTransformRules_MAX	UMETA(Hidden),
-};
-
-struct CSCORE_API EMCsAttachmentTransformRules : public TCsEnumMap<ECsAttachmentTransformRules>
-{
-	CS_ENUM_MAP_BODY_WITH_EXPLICIT_MAX(EMCsAttachmentTransformRules, ECsAttachmentTransformRules)
-};
-
-namespace NCsAttachmentTransformRules
-{
-	typedef ECsAttachmentTransformRules Type;
-
-	namespace Ref
-	{
-		extern CSCORE_API const Type KeepRelativeTransform;
-		extern CSCORE_API const Type KeepWorldTransform;
-		extern CSCORE_API const Type SnapToTargetNotIncludingScale;
-		extern CSCORE_API const Type SnapToTargetIncludingScale;
-		extern CSCORE_API const Type ECsAttachmentTransformRules_MAX;
-	}
-
-	extern CSCORE_API const uint8 MAX;
-
-	FORCEINLINE const FAttachmentTransformRules& ToRule(const Type& EType)
-	{
-		if (EType == Type::KeepRelativeTransform) { return FAttachmentTransformRules::KeepRelativeTransform; }
-		if (EType == Type::KeepWorldTransform) { return FAttachmentTransformRules::KeepWorldTransform; }
-		if (EType == Type::SnapToTargetNotIncludingScale) { return FAttachmentTransformRules::SnapToTargetNotIncludingScale; }
-		if (EType == Type::SnapToTargetIncludingScale) { return FAttachmentTransformRules::SnapToTargetIncludingScale; }
-		return FAttachmentTransformRules::KeepRelativeTransform;
-	}
-}
-
-#define ECS_ATTACHMENT_TRANSFORM_RULES_MAX NCsAttachmentTransformRules::MAX
-
-#pragma endregion AttachmentTransformRules
-
 // DetachmentTransformRules
 #pragma region
 

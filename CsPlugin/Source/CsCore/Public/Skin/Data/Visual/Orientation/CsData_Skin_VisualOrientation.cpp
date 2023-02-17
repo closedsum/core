2e7333fac207623acc06f63fe3032f541b7e2ba8
closedsum <closedsum@gmail.com>
2023-02-16 17:26:36 -0800

    Stubbing in Skin Orientation Data Interface

---
+++ CsPlugin/Source/CsCore/Public/Skin/Data/Visual/Orientation/CsData_Skin_VisualOrientation.cpp
@@ -0,0 +1,11 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Skin/Data/Visual/Orientation/CsData_Skin_VisualOrientation.h"
+#include "CsCore.h"
+
+const FName NCsSkin::NData::NVisual::NOrientation::IOrientation::Name = FName("NCsSkin::NData::NVisual::NOrientation::IOrientation");
+
+const FName ICsData_Skin_VisualOrientation::Name = FName("ICsData_Skin_VisualOrientation");
+
+UCsData_Skin_VisualOrientation::UCsData_Skin_VisualOrientation(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+}
---
+++ CsPlugin/Source/CsCore/Public/Skin/Data/Visual/Orientation/CsData_Skin_VisualOrientation.h
@@ -0,0 +1,62 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+// Interfaces
+#include "Containers/CsGetInterfaceMap.h"
+
+#include "CsData_Skin_VisualOrientation.generated.h"
+
+namespace NCsSkin
+{
+	namespace NData
+	{
+		namespace NVisual
+		{
+			namespace NOrientation
+			{
+				/**
+				* Interface for determining the Orientation (Rotation) of the visual Skin.
+				* The default Orientation is FRotator(0.0f, 0.0f, 0.0f).
+				*/
+				struct CSCORE_API IOrientation : public ICsGetInterfaceMap
+				{
+				public:
+
+					static const FName Name;
+
+				public:
+
+					virtual ~IOrientation() {}
+
+					/**
+					*/
+					virtual const FRotator& GetRotation() const = 0;
+				};
+			}
+		}
+	}
+}
+
+UINTERFACE(BlueprintType)
+class CSCORE_API UCsData_Skin_VisualOrientation : public UCsGetInterfaceMap
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+/**
+* Interface for determining the Orientation (Rotation) of the visual Skin.
+* The default Orientation is FRotator(0.0f, 0.0f, 0.0f).
+*/
+class CSCORE_API ICsData_Skin_VisualOrientation : public ICsGetInterfaceMap
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	static const FName Name;
+
+public:
+
+	/**
+	*/
+	virtual const FRotator& GetRotation() const = 0;
+};

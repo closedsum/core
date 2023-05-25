2867b38bb988dcab9545547d52642a8a33102c5d
closedsum <closedsum@gmail.com>
2023-05-24 17:38:16 -0700

    Adding Shutdown INterface

---
+++ CsPlugin/Source/CsCore/Public/Shutdown/CsShutdown.cpp
@@ -0,0 +1,9 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#include "Shutdown/CsShutdown.h"
+#include "CsCore.h"
+
+UCsShutdown::UCsShutdown(const class FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
---
+++ CsPlugin/Source/CsCore/Public/Shutdown/CsShutdown.h
@@ -0,0 +1,22 @@
+// Copyright 2017-2022 Closed Sum Games, LLC. All Rights Reserved.
+#pragma once
+#include "UObject/Interface.h"
+
+#include "CsShutdown.generated.h"
+
+UINTERFACE(BlueprintType, meta = (CannotImplementInterfaceInBlueprint))
+class CSCORE_API UCsShutdown : public UInterface
+{
+	GENERATED_UINTERFACE_BODY()
+};
+
+/**
+*/
+class CSCORE_API ICsShutdown
+{
+	GENERATED_IINTERFACE_BODY()
+
+public:
+
+	virtual void Shutdown() = 0;
+};

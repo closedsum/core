47ee8fa509c7e78e400f24313d3c324857ea81a5
closedsum <closedsum@gmail.com>
2023-07-18 23:29:43 -0700

    UPdating Projectile Mesh Info More heper Niagara functions

--- CsProjectile/Source/CsPrj/Public/Data/CsTypes_Data_Projectile.cpp
+++ CsProjectile/Source/CsPrj/Public/Data/CsTypes_Data_Projectile.cpp
@@ -36,6 +36,50 @@ bool FCsPrjStaticMesh::IsValid(const FString& Context, void(*Log)(const FString&
 	return true;
 }
 
+#define InfoType NCsProjectile::NVisual::NStaticMesh::FInfo
+
+void FCsProjectile_Visual_StaticMeshInfo::CopyToInfo(InfoType* Info)
+{
+	Info->SetMesh(Mesh.GetPtr());
+	Info->SetScale(&Scale);
+	Info->SetbScaleByCollisionRadius(&bScaleByCollisionRadius);
+	Info->SetScaleByCollisionRadiusMultiplier(&ScaleByCollisionRadiusMultiplier);
+}
+
+void FCsProjectile_Visual_StaticMeshInfo::CopyToInfoAsValue(InfoType* Info) const
+{
+	Info->SetMesh(Mesh.Get());
+	Info->SetScale(Scale);
+	Info->SetbScaleByCollisionRadius(bScaleByCollisionRadius);
+	Info->SetScaleByCollisionRadiusMultiplier(ScaleByCollisionRadiusMultiplier);
+}
+
+#undef InfoType
+
+bool FCsProjectile_Visual_StaticMeshInfo::IsValidChecked(const FString& Context) const
+{
+	CS_IS_VALID_CHECKED(Mesh);
+	CS_IS_VECTOR_COMPONENTS_GREATER_THAN_CHECKED(Scale, 0.0f)
+
+	if (bScaleByCollisionRadius)
+	{
+		CS_IS_FLOAT_GREATER_THAN_CHECKED(ScaleByCollisionRadiusMultiplier, 0.0f)
+	}
+	return true;
+}
+
+bool FCsProjectile_Visual_StaticMeshInfo::IsValid(const FString& Context, void(*Log)(const FString&) /*=&NCsProjectile::FLog::Warning*/) const
+{
+	CS_IS_VALID(Mesh)
+	CS_IS_VECTOR_COMPONENTS_GREATER_THAN(Scale, 0.0f)
+
+	if (bScaleByCollisionRadius)
+	{
+		CS_IS_FLOAT_GREATER_THAN(ScaleByCollisionRadiusMultiplier, 0.0f)
+	}
+	return true;
+}
+
 namespace NCsProjectile
 {
 	namespace NVisual
@@ -46,6 +90,11 @@ namespace NCsProjectile
 			{
 				CS_IS_PTR_NULL_CHECKED(GetMesh())
 				CS_IS_VECTOR_COMPONENTS_GREATER_THAN_CHECKED(GetScale(), 0.0f)
+
+				if (GetbScaleByCollisionRadius())
+				{
+					CS_IS_FLOAT_GREATER_THAN_CHECKED(GetScaleByCollisionRadiusMultiplier(), 0.0f)
+				}
 				return true;
 			}
 
@@ -53,6 +102,11 @@ namespace NCsProjectile
 			{
 				CS_IS_PTR_NULL(Mesh)
 				CS_IS_VECTOR_COMPONENTS_GREATER_THAN(GetScale(), 0.0f)
+
+				if (GetbScaleByCollisionRadius())
+				{
+					CS_IS_FLOAT_GREATER_THAN(GetScaleByCollisionRadiusMultiplier(), 0.0f)
+				}
 				return true;
 			}
 		}
--- CsProjectile/Source/CsPrj/Public/Data/CsTypes_Data_Projectile.h
+++ CsProjectile/Source/CsPrj/Public/Data/CsTypes_Data_Projectile.h
@@ -47,6 +47,51 @@ public:
 	bool IsValid(const FString& Context, void(*Log)(const FString&) = &NCsProjectile::FLog::Warning) const;
 };
 
+
+// NCsProjectile::NVisual::NStaticMesh::FInfo
+CS_FWD_DECLARE_STRUCT_NAMESPACE_3(NCsProjectile, NVisual, NStaticMesh, FInfo)
+
+USTRUCT(BlueprintType)
+struct CSPRJ_API FCsProjectile_Visual_StaticMeshInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CsPrj|Mesh")
+	FCsStaticMesh Mesh;
+
+	/** The size of the Mesh in World Space. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CsPrj|Mesh")
+	FVector Scale;
+
+	/** Whether to scale the mesh by the Collision Radius. 
+		NOTE: If TRUE, Scale is ignored. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CsPrj|Mesh")
+	bool bScaleByCollisionRadius;
+
+	/** Multiplier to apply to Collision Radius.
+		NOTE: Only used if bScaleByCollisionRadius is TRUE. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "CsPrj|Mesh", meta = (EditCondition = "bScaleByCollisionRadius", UIMin = "0.0", ClampMin = "0.0"))
+	float ScaleByCollisionRadiusMultiplier;
+
+public:
+
+	FCsProjectile_Visual_StaticMeshInfo() :
+		Mesh(),
+		Scale(1.0f),
+		bScaleByCollisionRadius(false),
+		ScaleByCollisionRadiusMultiplier(1.0f)
+	{
+	}
+
+#define InfoType NCsProjectile::NVisual::NStaticMesh::FInfo
+	void CopyToInfo(InfoType* Info);
+	void CopyToInfoAsValue(InfoType* Info) const;
+#undef InfoType
+
+	bool IsValidChecked(const FString& Context) const;
+	bool IsValid(const FString& Context, void(*Log)(const FString&) = &NCsProjectile::FLog::Warning) const;
+};
+
 class UStaticMesh;
 
 namespace NCsProjectile
@@ -60,24 +105,33 @@ namespace NCsProjectile
 			private:
 			
 				CS_DECLARE_MEMBER_WITH_PROXY(Mesh, UStaticMesh*)
+				/** The size of the Mesh in World Space. */
 				CS_DECLARE_MEMBER_WITH_PROXY(Scale, FVector)
+				/** Whether to scale the mesh by the Collision Radius. 
+					NOTE: If TRUE, Scale is ignored. */
 				CS_DECLARE_MEMBER_WITH_PROXY(bScaleByCollisionRadius, bool)
+				/** Multiplier to apply to Collision Radius.
+					NOTE: Only used if bScaleByCollisionRadius is TRUE. */
+				CS_DECLARE_MEMBER_WITH_PROXY(ScaleByCollisionRadiusMultiplier, float)
 
 			public:
 
 				FInfo() :
 					CS_CTOR_INIT_MEMBER_WITH_PROXY(Mesh, nullptr),
 					CS_CTOR_INIT_MEMBER_WITH_PROXY(Scale, 1.0f),
-					CS_CTOR_INIT_MEMBER_WITH_PROXY(bScaleByCollisionRadius, false)
+					CS_CTOR_INIT_MEMBER_WITH_PROXY(bScaleByCollisionRadius, false),
+					CS_CTOR_INIT_MEMBER_WITH_PROXY(ScaleByCollisionRadiusMultiplier, 1.0f)
 				{
 					CS_CTOR_SET_MEMBER_PROXY(Mesh);
 					CS_CTOR_SET_MEMBER_PROXY(Scale);
 					CS_CTOR_SET_MEMBER_PROXY(bScaleByCollisionRadius);
+					CS_CTOR_SET_MEMBER_PROXY(ScaleByCollisionRadiusMultiplier);
 				}
 	
 				CS_DEFINE_SET_GET_MEMBER_PTR_WITH_PROXY(Mesh, UStaticMesh)
 				CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(Scale, FVector)
 				CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(bScaleByCollisionRadius, bool)
+				CS_DEFINE_SET_GET_MEMBER_WITH_PROXY(ScaleByCollisionRadiusMultiplier, float)
 
 				bool IsValidChecked(const FString& Context) const;
 				bool IsValid(const FString& Context, void(*Log)(const FString&) = &NCsProjectile::FLog::Warning) const;
@@ -86,4 +140,4 @@ namespace NCsProjectile
 	}
 }
 
-#pragma endregion FCsPrjStaticMesh
+#pragma endregion FCsProjectile_Visual_StaticMeshInfo

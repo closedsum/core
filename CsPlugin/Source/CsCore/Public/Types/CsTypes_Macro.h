c423bfcf9f50a17a04b017d0fa1a04ab468073bb
closedsum <closedsum@gmail.com>
2023-08-17 23:16:01 -0700

    Updating license

--- CsPlugin/Source/CsCore/Public/Types/CsTypes_Macro.h
+++ CsPlugin/Source/CsCore/Public/Types/CsTypes_Macro.h
@@ -1,5 +1,6 @@
 // Copyright 2017-2023 Closed Sum Games, LLC. All Rights Reserved.
-//#include "CsTypes_Macro.generated.h"
+// MIT License: https://opensource.org/license/mit/
+// Free for use and distribution: https://github.com/closedsum/core
 #pragma once
 
 #define CS_EMPTY 0

751ebe4ed2b66553acaa689198cce97a5f039d2e
closedsum <closedsum@gmail.com>
2023-09-09 11:54:07 -0700

    Updating license

--- CsPlugin/Source/CsCore/Public/CsCore.h
+++ CsPlugin/Source/CsCore/Public/CsCore.h
@@ -1,4 +1,6 @@
 // Copyright 2017-2023 Closed Sum Games, LLC. All Rights Reserved.
+// MIT License: https://opensource.org/license/mit/
+// Free for use and distribution: https://github.com/closedsum/core
 #ifndef __CSCORE_H__
 #define __CSCORE_H__
 

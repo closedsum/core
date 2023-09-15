46a08707340e13048a38c53d0cf65171eba05be6
closedsum <closedsum@gmail.com>
2023-09-15 08:45:56 -0700

    Fixing weird git merge

--- CsAI/Source/CsAI/Public/Utility/CsAILog.h
+++ CsAI/Source/CsAI/Public/Utility/CsAILog.h
@@ -1,12 +1,7 @@
 // Copyright 2017-2023 Closed Sum Games, LLC. All Rights Reserved.
 // MIT License: https://opensource.org/license/mit/
 // Free for use and distribution: https://github.com/closedsum/core
-<<<<<<<< HEAD:CsAI/Source/CsAI/Public/Utility/CsAILog.h
 #pragma once
-========
-#include "Managers/Sense/CsSensingObject.h"
-#include "CsSense.h"
->>>>>>>> origin/master:CsSense/Source/CsSense/Public/Managers/Sense/CsSensingObject.cpp
 
 namespace NCsAI
 {

043d1f237263d54cf1a70cc6ad9c33d034fc51a3
closedsum <closedsum@gmail.com>
2024-09-05 12:59:02 -0700

    code cleanup

--- CsFX/Source/CsFX/CsFX.Build.cs
+++ CsFX/Source/CsFX/CsFX.Build.cs
@@ -34,6 +34,8 @@ public class CsFX : ModuleRules
                     "CsContainer",
                     "CsEnum",
                     "CsValid",
+                    "CsManager",
+                    "CsScopedTimer",
                     "CsTypes",
                     "CsCoreLibrary",
                     "CsTime",
